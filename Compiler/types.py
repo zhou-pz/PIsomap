0343617f6619797635a2154b94ec756e9e3b5370
Marcel Keller <mks.keller@gmail.com>
2024-03-25 13:45:36 +1100

    Fix bug in tensor input.

--- Compiler/types.py
+++ Compiler/types.py
@@ -620,7 +620,7 @@ class _secret_structure(_structure):
                 content = numpy.array(content)
                 if issubclass(cls, _fix):
                     min_k = \
-                        math.ceil(math.log(abs(content).max(), 2)) + cls.f + 1
+                        math.ceil(math.log(abs(content).max() or 1, 2)) + cls.f + 1
                     if cls.k < min_k:
                         raise CompilerError(
                             "data outside fixed-point range, "
