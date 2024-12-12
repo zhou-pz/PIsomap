0faee7dcb026da406f84deda4e3b2bfe608467c3
Vincent Ehrmanntraut <ehrmanntraut@itsec.rwth-aachen.de>
2024-12-12 10:47:23 +0100

    Implement shuffle for SpdzWise Rep3

--- Protocols/Rep3Shuffler.hpp
+++ Protocols/Rep3Shuffler.hpp
@@ -9,34 +9,27 @@
 #include "Rep3Shuffler.h"
 
 template<class T>
-Rep3Shuffler<T>::Rep3Shuffler(StackedVector<T>& a, size_t n, int unit_size,
-        size_t output_base, size_t input_base, SubProcessor<T>& proc) :
-        proc(proc)
-{
+Rep3Shuffler<T>::Rep3Shuffler(StackedVector<T> &a, size_t n, int unit_size,
+                              size_t output_base, size_t input_base, SubProcessor<T> &proc) : proc(proc) {
     store_type store;
     int handle = generate(n / unit_size, store);
     apply(a, n, unit_size, output_base, input_base, store.get(handle),
-            false);
+          false);
 }
 
 template<class T>
-Rep3Shuffler<T>::Rep3Shuffler(SubProcessor<T>& proc) :
-        proc(proc)
-{
+Rep3Shuffler<T>::Rep3Shuffler(SubProcessor<T> &proc) : proc(proc) {
 }
 
 template<class T>
-int Rep3Shuffler<T>::generate(int n_shuffle, store_type& store)
-{
+int Rep3Shuffler<T>::generate(int n_shuffle, store_type &store) {
     int res = store.add();
-    auto& shuffle = store.get(res);
-    for (int i = 0; i < 2; i++)
-    {
-        auto& perm = shuffle[i];
+    auto &shuffle = store.get(res);
+    for (int i = 0; i < 2; i++) {
+        auto &perm = shuffle[i];
         for (int j = 0; j < n_shuffle; j++)
             perm.push_back(j);
-        for (int j = 0; j < n_shuffle; j++)
-        {
+        for (int j = 0; j < n_shuffle; j++) {
             int k = proc.protocol.shared_prngs[i].get_uint(n_shuffle - j);
             swap(perm[j], perm[k + j]);
         }
@@ -45,25 +38,26 @@ int Rep3Shuffler<T>::generate(int n_shuffle, store_type& store)
 }
 
 template<class T>
-void Rep3Shuffler<T>::apply(StackedVector<T>& a, size_t n, int unit_size,
-        size_t output_base, size_t input_base, shuffle_type& shuffle,
-        bool reverse)
-{
-    vector<size_t> sizes { n };
-    vector<size_t> unit_sizes { static_cast<size_t>(unit_size) };
-    vector<size_t> destinations { output_base };
-    vector<size_t> sources { input_base };
-    vector<shuffle_type> shuffles { shuffle };
-    vector<bool> reverses { reverse };
+void Rep3Shuffler<T>::apply(StackedVector<T> &a, size_t n, int unit_size,
+                            size_t output_base, size_t input_base, shuffle_type &shuffle,
+                            bool reverse) {
+    vector<size_t> sizes{n};
+    vector<size_t> unit_sizes{static_cast<size_t>(unit_size)};
+    vector<size_t> destinations{output_base};
+    vector<size_t> sources{input_base};
+    vector<shuffle_type> shuffles{shuffle};
+    vector<bool> reverses{reverse};
     this->applyMultiple(a, sizes, unit_sizes, destinations, sources, shuffles, reverses);
 }
 
 template<class T>
-void Rep3Shuffler<T>::applyMultiple(StackedVector<T>& a, vector<size_t>& sizes, vector<size_t>& destinations, vector<size_t>& sources,
-                                    vector<size_t>& unit_sizes, vector<size_t>& handles, vector<bool>& reverses, store_type& store) {
+void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, vector<size_t> &destinations,
+                                    vector<size_t> &sources,
+                                    vector<size_t> &unit_sizes, vector<size_t> &handles, vector<bool> &reverses,
+                                    store_type &store) {
     vector<shuffle_type> shuffles;
-    for (size_t &handle : handles) {
-        shuffle_type& shuffle = store.get(handle);
+    for (size_t &handle: handles) {
+        shuffle_type &shuffle = store.get(handle);
         shuffles.push_back(shuffle);
     }
 
@@ -72,8 +66,8 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T>& a, vector<size_t>& sizes, 
 
 template<class T>
 void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, vector<size_t> &destinations,
-    vector<size_t> &sources, vector<size_t> &unit_sizes, vector<shuffle_type> &shuffles, vector<bool> &reverses)
-{
+                                    vector<size_t> &sources, vector<size_t> &unit_sizes, vector<shuffle_type> &shuffles,
+                                    vector<bool> &reverses) {
     const auto n_shuffles = sizes.size();
     assert(sources.size() == n_shuffles);
     assert(destinations.size() == n_shuffles);
@@ -86,10 +80,10 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, 
     assert(not T::dishonest_majority);
 
     // for (size_t i = 0; i < n_shuffles; i++) {
-        // this->apply(a, sizes[i], unit_sizes[i], destinations[i], sources[i], store.get(handles[i]), reverses[i]);
+    // this->apply(a, sizes[i], unit_sizes[i], destinations[i], sources[i], store.get(handles[i]), reverses[i]);
     // }
 
-    vector<vector<T>> to_shuffle;
+    vector<vector<T> > to_shuffle;
     for (size_t current_shuffle = 0; current_shuffle < n_shuffles; current_shuffle++) {
         assert(sizes[current_shuffle] % unit_sizes[current_shuffle] == 0);
         vector<T> x;
@@ -97,7 +91,7 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, 
             x.push_back(a[sources[current_shuffle] + j]);
         to_shuffle.push_back(x);
 
-        const auto& shuffle = shuffles[current_shuffle];
+        const auto &shuffle = shuffles[current_shuffle];
         if (shuffle.empty())
             throw runtime_error("shuffle has been deleted");
 
@@ -106,50 +100,47 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, 
 
     typename T::Input input(proc);
 
-    for (int pass = 0; pass < 3; pass++)
-    {
-    input.reset_all(proc.P);
+    for (int pass = 0; pass < 3; pass++) {
+        input.reset_all(proc.P);
 
-    for (size_t current_shuffle = 0; current_shuffle < n_shuffles; current_shuffle++) {
-        const auto n = sizes[current_shuffle];
-        const auto unit_size = unit_sizes[current_shuffle];
-        const auto& shuffle = shuffles[current_shuffle];
-        const auto reverse = reverses[current_shuffle];
-        const auto current_to_shuffle = to_shuffle[current_shuffle];
+        for (size_t current_shuffle = 0; current_shuffle < n_shuffles; current_shuffle++) {
+            const auto n = sizes[current_shuffle];
+            const auto unit_size = unit_sizes[current_shuffle];
+            const auto &shuffle = shuffles[current_shuffle];
+            const auto reverse = reverses[current_shuffle];
+            const auto current_to_shuffle = to_shuffle[current_shuffle];
 
-        vector<typename T::clear> to_share(n);
-        int i;
-        if (reverse)
-            i = 2 - pass;
-        else
-            i = pass;
+            vector<typename T::clear> to_share(n);
+            int i;
+            if (reverse)
+                i = 2 - pass;
+            else
+                i = pass;
 
-        if (proc.P.get_player(i) == 0) {
-            for (size_t j = 0; j < n / unit_size; j++)
-                for (size_t k = 0; k < unit_size; k++)
-                    if (reverse)
-                        to_share.at(j * unit_size + k) = current_to_shuffle.at(
-                            shuffle[0].at(j) * unit_size + k).sum();
-                    else
-                        to_share.at(shuffle[0].at(j) * unit_size + k) =
-                                current_to_shuffle.at(j * unit_size + k).sum();
+            if (proc.P.get_player(i) == 0) {
+                for (size_t j = 0; j < n / unit_size; j++)
+                    for (size_t k = 0; k < unit_size; k++)
+                        if (reverse)
+                            to_share.at(j * unit_size + k) = current_to_shuffle.at(
+                                shuffle[0].at(j) * unit_size + k).sum();
+                        else
+                            to_share.at(shuffle[0].at(j) * unit_size + k) =
+                                    current_to_shuffle.at(j * unit_size + k).sum();
+            } else if (proc.P.get_player(i) == 1) {
+                for (size_t j = 0; j < n / unit_size; j++)
+                    for (size_t k = 0; k < unit_size; k++)
+                        if (reverse)
+                            to_share[j * unit_size + k] = current_to_shuffle[shuffle[1][j] * unit_size + k][0];
+                        else
+                            to_share[shuffle[1][j] * unit_size + k] = current_to_shuffle[j * unit_size + k][0];
+            }
+
+            if (proc.P.get_player(i) < 2)
+                for (auto &x: to_share)
+                    input.add_mine(x);
+            for (int k = 0; k < 2; k++)
+                input.add_other((-i + 3 + k) % 3);
         }
-        else if (proc.P.get_player(i) == 1)
-        {
-            for (size_t j = 0; j < n / unit_size; j++)
-                for (size_t k = 0; k < unit_size; k++)
-                    if (reverse)
-                        to_share[j * unit_size + k] = current_to_shuffle[shuffle[1][j] * unit_size + k][0];
-                    else
-                        to_share[shuffle[1][j] * unit_size + k] = current_to_shuffle[j * unit_size + k][0];
-        }
-
-        if (proc.P.get_player(i) < 2)
-            for (auto& x : to_share)
-                input.add_mine(x);
-        for (int k = 0; k < 2; k++)
-            input.add_other((-i + 3 + k) % 3);
-    }
 
         input.exchange();
         to_shuffle.clear();
@@ -165,8 +156,7 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, 
                 i = pass;
 
             vector<T> tmp;
-            for (size_t j = 0; j < n; j++)
-            {
+            for (size_t j = 0; j < n; j++) {
                 T x = input.finalize((-i + 3) % 3) + input.finalize((-i + 4) % 3);
                 tmp.push_back(x);
             }
@@ -183,8 +173,7 @@ void Rep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, 
 }
 
 template<class T>
-void Rep3Shuffler<T>::inverse_permutation(StackedVector<T>&, size_t, size_t, size_t)
-{
+void Rep3Shuffler<T>::inverse_permutation(StackedVector<T> &, size_t, size_t, size_t) {
     throw runtime_error("inverse permutation not implemented");
 }
 
--- Protocols/SpdzWiseRep3Shuffler.hpp
+++ Protocols/SpdzWiseRep3Shuffler.hpp
@@ -60,6 +60,70 @@ void SpdzWiseRep3Shuffler<T>::apply(StackedVector<T>& a, size_t n,
     proc.protocol.maybe_check();
 }
 
+
+template<class T>
+void SpdzWiseRep3Shuffler<T>::applyMultiple(StackedVector<T>& a, vector<size_t>& sizes, vector<size_t>& destinations, vector<size_t>& sources,
+                                    vector<size_t>& unit_sizes, vector<size_t>& handles, vector<bool>& reverses, store_type& store) {
+    vector<shuffle_type> shuffles;
+    for (size_t &handle : handles) {
+        shuffle_type& shuffle = store.get(handle);
+        shuffles.push_back(shuffle);
+    }
+
+    applyMultiple(a, sizes, destinations, sources, unit_sizes, shuffles, reverses);
+}
+
+
+template<class T>
+void SpdzWiseRep3Shuffler<T>::applyMultiple(StackedVector<T> &a, vector<size_t> &sizes, vector<size_t> &destinations,
+    vector<size_t> &sources, vector<size_t> &unit_sizes, vector<shuffle_type> &shuffles, vector<bool> &reverse) {
+
+    const size_t n_shuffles = sizes.size();
+    assert(n_shuffles == destinations.size());
+    assert(n_shuffles == sources.size());
+    assert(n_shuffles == unit_sizes.size());
+    assert(n_shuffles == shuffles.size());
+    assert(n_shuffles == reverse.size());
+
+    StackedVector<typename T::part_type::Honest> temporary_memory(0);
+    vector<size_t> mapped_positions (n_shuffles, 0);
+    vector<size_t> mapped_sizes(n_shuffles, 0);
+    vector<size_t> mapped_unit_sizes (n_shuffles, 0);
+
+    for (size_t current_shuffle = 0; current_shuffle < n_shuffles; current_shuffle++) {
+        mapped_positions[current_shuffle] = temporary_memory.size();
+
+        mapped_sizes[current_shuffle] = 2 * sizes[current_shuffle];
+        mapped_unit_sizes[current_shuffle] = 2 * unit_sizes[current_shuffle];
+        stats[sizes[current_shuffle] / unit_sizes[current_shuffle]] += unit_sizes[current_shuffle];
+
+        for (size_t i = 0; i < sizes[current_shuffle]; i++)
+        {
+            auto& x = a[sources[current_shuffle] + i];
+            temporary_memory.push_back(x.get_share());
+            temporary_memory.push_back(x.get_mac());
+        }
+    }
+
+    internal.applyMultiple(temporary_memory, mapped_sizes, mapped_positions, mapped_positions, mapped_unit_sizes, shuffles, reverse);
+
+    for (size_t current_shuffle = 0; current_shuffle < n_shuffles; current_shuffle++)
+    {
+        const size_t n = sizes[current_shuffle];
+        const size_t dest = destinations[current_shuffle];
+        const size_t pos = mapped_positions[current_shuffle];
+        for (size_t i = 0; i < n; i++)
+        {
+            auto& x = a[dest + i];
+            x.set_share(temporary_memory[pos + 2 * i]);
+            x.set_mac(temporary_memory[pos + 2 * i + 1]);
+            proc.protocol.add_to_check(x);
+        }
+    }
+
+    proc.protocol.maybe_check();
+}
+
 template<class T>
 void SpdzWiseRep3Shuffler<T>::inverse_permutation(StackedVector<T>&, size_t, size_t,
         size_t)
